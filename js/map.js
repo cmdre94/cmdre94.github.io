b9a1e621a8997ec5d97525a335fdf2749ed3325c
cmdre94 <cmdre94@users.noreply.github.com>
2016-04-27 17:00:09 -0500

    Google Map with HTTPS
    Served google map over https

--- js/map.js
+++ js/map.js
@@ -16,7 +16,7 @@
 }*/
 
 /*
-This is the fun part. Here's where we generate the custom Google Map for the website.
+Generates a custom Google Map for the website.
 See the documentation below for more details.
 https://developers.google.com/maps/documentation/javascript/reference
 */
@@ -24,7 +24,7 @@ https://developers.google.com/maps/documentation/javascript/reference
 var map;    // declares a global map variable
 
 /*
-Start here! initializeMap() is called when page is loaded.
+initializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
@@ -35,22 +35,20 @@ function initializeMap() {
   };
 
   /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
+  locationFinder() returns an array of every location.
   */
   function locationFinder() {
 
     // initializes an empty array
     var locations = [];
 
-    // iterates through school locations and appends each location to
+    // iterates all locations and appends each location to
     // the locations array
     for (var address in initialLocations) {
       locations.push(initialLocations[address].address);
